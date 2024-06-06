43b5c67a451d37ee4973e78fdb9de88cc4b32b08
Marcos Cereijo <marcos.rodriguez@pexip.com>
2024-06-06 15:51:24 +0200

    chore: first version

---
+++ README.md
@@ -0,0 +1,109 @@
+# Web App 3 Plugin: Reactions
+
+This plugin gives the user possibility to share reactions with the rest of
+participants in a conference. We have three different reactions: applause, love
+and laugh.
+
+For using this plugin you will need **Pexip Infinity v35** or later.
+
+## How to use
+
+The plugin will create a new button on the toolbar that any user can use during
+a conference to share reactions with the rest of the participants:
+
+<div align='center'>
+
+![button](./images/button.png)
+
+</div>
+
+- Once the user click on the button, they will see a toast message on top:
+
+<div align='center'>
+
+![button](./images/you-reacted.png)
+
+</div>
+
+- The rest of the participants will see a similar message:
+
+<div align='center'>
+
+![button](./images/somebody-reacted.png)
+
+</div>
+
+- An animation will be displayed for all the participants on top of the video:
+
+<div align='center'>
+
+![button](./images/conference.png)
+
+</div>
+
+## Run for development
+
+- To be able to build the plugin, you need to comply with the following versions
+  or higher:
+
+  | NodeJS   | NPM     |
+  | -------- | ------- |
+  | v20.12.2 | v10.5.0 |
+
+- Install all the dependencies:
+
+  ```bash
+  $ npm i
+  ```
+
+- Before starting the dev server, you need to modify the configuration
+  `vite.json`:
+
+  You can modify the following parameters:
+
+  - `infinityUrl`: URL for the Pexip Infinity Conferencing Node.
+  - `port`: Port to use for the developer server.
+  - `brandingPath`: Path for the branding configured in Infinity.
+
+  Example:
+
+  ```json
+  {
+    "infinityUrl": "https://pexipdemo.com",
+    "port": 5173,
+    "brandingPath": "/local-plugin"
+  }
+  ```
+
+- Run the dev environment:
+
+  ```bash
+  $ npm start
+  ```
+
+The plugin will be served from https://localhost:5173 (visit that page and
+accept the self-signed certificates), but you should access it thought the Web
+App 3 URL. You have more information about how to configure your environment in
+the
+[Developer Portal: Setup guide for plugin developers](https://developer.pexip.com/docs/plugins/webapp-3/setup-guide-for-plugin-developers).
+
+## Build for production
+
+To create a package, you will need to first install all the dependencies:
+
+```bash
+$ npm i
+```
+
+And now to create the package itself:
+
+```bash
+$ npm run build
+```
+
+Congrats! Your package is ready and it will be available in the `dist` folder.
+The next step is to create a Web App3 branding and copy `dist` into that
+branding.
+
+If you want to know more about how to deploy your plugin in Pexip Infinity,
+check our [Developer Portal](https://developer.pexip.com).
---
+++ src/index.ts
@@ -0,0 +1,118 @@
+import { registerPlugin } from '@pexip/plugin-api'
+import { Emoji, IconAscii, IconSVG } from './Emoji'
+
+const plugin = await registerPlugin({
+  id: 'reactions',
+  version: 0
+})
+
+const animationDuration = 7000
+
+const button = await plugin.ui.addButton({
+  position: 'toolbar',
+  tooltip: Emoji.Reactions,
+  icon: IconSVG.Reactions,
+  group: [
+    {
+      id: Emoji.Applause,
+      position: 'toolbar',
+      tooltip: Emoji.Applause,
+      icon: IconSVG.Applause
+    },
+    {
+      id: Emoji.Love,
+      position: 'toolbar',
+      tooltip: Emoji.Love,
+      icon: IconSVG.Love
+    },
+    {
+      id: Emoji.Laugh,
+      position: 'toolbar',
+      tooltip: Emoji.Laugh,
+      icon: IconSVG.Laugh
+    }
+  ]
+})
+
+button.onClick.add(async ({ buttonId }) => {
+  try {
+    await plugin.conference.sendApplicationMessage({
+      payload: {
+        reaction: buttonId
+      }
+    })
+    await plugin.ui.showToast({
+      message: `You reacted with ${getAsciiIcon(buttonId)}`,
+      isInterrupt: true
+    })
+    showReactionGif(buttonId)
+  } catch (e) {
+    console.error(e)
+  }
+})
+
+plugin.events.applicationMessage.add(async (appMessage) => {
+  const reaction = appMessage.message.reaction as string
+  const reactionIcon = getAsciiIcon(reaction)
+  await plugin.ui.showToast({
+    message: `${appMessage.displayName} reacted with ${reactionIcon}`,
+    isInterrupt: true
+  })
+  showReactionGif(reaction)
+})
+
+const getAsciiIcon = (reaction: string): string => {
+  let reactionIcon: string
+  switch (reaction) {
+    case Emoji.Applause:
+      reactionIcon = IconAscii.Applause
+      break
+    case Emoji.Love:
+      reactionIcon = IconAscii.Love
+      break
+    case Emoji.Laugh:
+      reactionIcon = IconAscii.Laugh
+      break
+    default:
+      throw new Error('Received invalid reaction')
+  }
+  return reactionIcon
+}
+
+const showReactionGif = (icon: string): void => {
+  const basePath = getBasePath()
+
+  const gif = document.createElement('img')
+  gif.src = `${basePath}/images/${icon.toLowerCase()}.gif`
+  gif.className = 'reaction-gif'
+  gif.style.left = `${Math.random() * 100}%`
+  gif.style.bottom = '0'
+
+  const root = parent.document.querySelector(
+    'div[data-testid="in-meeting-video-wrapper"]'
+  )
+
+  if (root === null) {
+    throw new Error(
+      'Could not find video wrapper to attach the reaction gif to.'
+    )
+  }
+
+  root.appendChild(gif)
+
+  setTimeout(() => {
+    gif.remove()
+  }, animationDuration)
+}
+
+const getBasePath = (): string => {
+  return window.location.pathname.slice(
+    0,
+    window.location.pathname.lastIndexOf('/')
+  )
+}
+
+const style = document.createElement('link')
+style.rel = 'stylesheet'
+style.href = getBasePath() + '/style.css'
+parent.document.head.appendChild(style)
